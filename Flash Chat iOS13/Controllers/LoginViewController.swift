89115ac395ca37b9f79fc9b53b4c2f0e8e261ca2
ccdtruong <ccdtruong@gmail.com>
2024-09-19 13:52:07 +0700

    + add Constant file

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -23,7 +23,7 @@ class LoginViewController: UIViewController {
                     print(e)
                 }
                 else {
-                    self?.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    self?.performSegue(withIdentifier: K.loginSegue, sender: self)
                 }
             }
         }
